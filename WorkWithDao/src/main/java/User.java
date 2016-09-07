cfbe6719090780a6e878b234422981a11937ed91
Natalia Spiridonova <natusic199797@gmail.com>
2016-09-07 17:08:50 +0300

    New

---
+++ WorkWithDao/src/main/java/User.java
@@ -0,0 +1,33 @@
+/**
+ * Created by Natalia on 05.09.16.
+ */
+public class User implements UserInterface{
+    private String name;
+    private int id;
+
+    public User(String name, int id) {
+
+        this.name = name;
+        this.id = id;
+    }
+
+
+
+    public String getName() {
+
+        return this.name;
+
+    }
+
+
+    public int getId() {
+
+        return this.id;
+
+    }
+
+    public void setId(int id) {
+        
+        this.id = id;
+    }
+}
